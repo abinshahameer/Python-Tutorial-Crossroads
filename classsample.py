074eef5c7f3782ace89f181aa577441d50d92cde
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-06-27 11:57:43 +0530

    class tutorial

--- classsample.py
+++ classsample.py
@@ -1,8 +1,18 @@
 class MySampleClass:
     def hello(self, n):
+        self.name = n
         print("hello " + n)
+
+    def printname(self):
+        print(self.name)
+
+    def __init__(self):
+        print('hello')
 
 
 x = MySampleClass()
+y = MySampleClass()
 name = "abinshah"
 x.hello(name)
+x.printname()
+y.hello("heloo")
