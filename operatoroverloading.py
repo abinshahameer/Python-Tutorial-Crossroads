5ed62c78a492b6c8e5edd3ac6c7604e53e8ca976
Abinshah Ameer <abinshahcoolz@gmail.com>
2020-06-27 22:50:27 +0530

    operator overloading

---
+++ operatoroverloading.py
@@ -0,0 +1,15 @@
+class sample:
+    def set_name(self, name):
+        self.name = name
+
+    def __add__(self, other):
+        name = self.name + other.name
+        return name
+
+
+firstname = sample()
+secondname = sample()
+firstname.set_name("abinshah")
+secondname.set_name("ameer")
+fullname = firstname + secondname
+print(fullname)
